5dedaa2ad777ed422af65c5be0ee9a6eeaae385f
parthasarathy <ben@ninositsolution.com>
2021-04-24 12:16:50 +0530

    item decoration has been added to recyclerview

--- app/src/main/java/com/pbsarathy21/trendingrepos/ui/TrendingRepoActivity.kt
+++ app/src/main/java/com/pbsarathy21/trendingrepos/ui/TrendingRepoActivity.kt
@@ -3,6 +3,10 @@ package com.pbsarathy21.trendingrepos.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.pbsarathy21.trendingrepos.R
 import com.pbsarathy21.trendingrepos.databinding.ActivityTrendingRepoBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,9 +16,20 @@ class TrendingRepoActivity : AppCompatActivity() {
     private val viewModel by viewModels<TrendingRepoViewModel>()
     private lateinit var binding: ActivityTrendingRepoBinding
 
+    private val itemDecoration by lazy {
+        DividerItemDecoration(this, DividerItemDecoration.VERTICAL).also {
+            ContextCompat.getDrawable(this, R.drawable.divider)?.let { it1 ->
+                it.setDrawable(it1)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTrendingRepoBinding.inflate(layoutInflater)
+        binding.repoList.hasFixedSize()
+        binding.repoList.layoutManager = LinearLayoutManager(this)
+        binding.repoList.addItemDecoration(itemDecoration)
         setContentView(binding.root)
     }
 }
